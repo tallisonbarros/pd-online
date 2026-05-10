330e67b172faa0f8f96f44518f511c767c4e9ee2
TallisonBarros <tallisonbarros@outlook.com>
2026-05-10 20:41:37 -0300

    Serve media files em producao

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve as serve_media
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,3 +11,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve_media, {"document_root": settings.MEDIA_ROOT}),
+    ]
